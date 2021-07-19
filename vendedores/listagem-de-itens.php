6d8895e8a98362823aa19f8ade35b29e0b3af675
Felipe Prodóssimo <73621753+FelipeProdossimo@users.noreply.github.com>
2021-07-19 16:53:29 -0300

    Pagina de itens quase pronta.

--- vendedores/listagem-de-itens.php
+++ vendedores/listagem-de-itens.php
@@ -38,6 +38,389 @@ include "../include/cabecalho-vendedores.php";
       echo "</div>";
       };
       ?>
-      
+<main class="mb-5 pb-5 mb-md-0">
+  <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-5">
+              <form class="justify-content-center justify-content-md-start mb-3 mb-md-0">
+                <div class="input-group input-group-sm">
+                  <input type="text" class="form-control" placegolder="Digite aqui o que procura">
+                  <button class="btn btn-danger">
+                    Buscar
+                  </button>
+                </div>
+              </form>
+            </div>
+            <div class="col-12 col-md-7">
+              <div class="d-flex flex-row-reverse justify-content-center justify-content-md-start">
+                <form class="ml-3 d-inline-block">
+                  <select class="form-select form-select-sm">
+                    <option>Ordenar pelo nome</option>
+                    <option>Ordenar pelo maior preço</option>
+                    <option>Ordenar pelo menor preço</option>
+                  </select>
+                </form>
+                <nav class="d-inline-block">
+                  <ul class="pagination pagination-sm my-0">
+                    <li class="page-item disabled">
+                      <button class="page-link">1</button>
+                    </li>
+                    <li class="page-item">
+                      <button class="page-link">2</button>
+                    </li>
+                    <li class="page-item">
+                      <button class="page-link">3</button>
+                    </li>
+                    <li class="page-item">
+                      <button class="page-link">4</button>
+                    </li>
+                    <li class="page-item">
+                      <button class="page-link">5</button>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+        </div>
+        <hr class="mt-3">
+        <div class="row g-3">
+          <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6 d-flex align-items-stretch">
+            <div class="card text-center bg-light">
+              <a href="" class="position-absolute right-0 p-2 text-danger">
+                <i class="bi bi-suit-heart"></i>
+              </a>
+              <img src="../img/Invocacao_Do_Mal.jpg" alt="Invocacao do mal" class="card-img-top">
+              <div class="card-header">
+                R$ 500,00
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">Banana Prata</h5>
+                <p class="card-text truncate-3l">
+                  Banana prata direto da terra
+                </p>
+              </div>
+              <div class="card-footer">
+                <form class="d-block">
+                  <button class="btn btn-danger">
+                    Add ao Carrinho
+                  </button>
+                </form>
+                <small class="text-success">320,00kg em estoque</small>
+              </div>
+            </div> 
+          </div>
+          <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6 d-flex align-items-stretch">
+            <div class="card text-center bg-light">
+              <a href="" class="position-absolute right-0 p-2 text-danger">
+                <i class="bi bi-suit-heart-fill"></i>
+              </a>
+              <img src="../img/Invocacao_Do_Mal.jpg" alt="Invocacao do mal" class="card-img-top">
+              <div class="card-header">
+                R$ 500,00
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">Banana Prata</h5>
+                <p class="card-text truncate-3l">
+                  Banana prata direto da terra
+                </p>
+              </div>
+              <div class="card-footer">
+                <form class="d-block">
+                  <button class="btn btn-danger">
+                    Add ao Carrinho
+                  </button>
+                </form>
+                <small class="text-success">320,00kg em estoque</small>
+              </div>
+            </div> 
+          </div>
+          <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6 d-flex align-items-stretch">
+            <div class="card text-center bg-light">
+              <a href="" class="position-absolute right-0 p-2 text-danger">
+                <i class="bi bi-suit-heart"></i>
+              </a>
+              <img src="../img/Invocacao_Do_Mal.jpg" alt="Invocacao do mal" class="card-img-top">
+              <div class="card-header">
+                R$ 500,00
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">Banana Prata</h5>
+                <p class="card-text truncate-3l">
+                  Banana prata direto da terra
+                </p>
+              </div>
+              <div class="card-footer">
+                <form class="d-block">
+                  <button class="btn btn-danger">
+                    Add ao Carrinho
+                  </button>
+                </form>
+                <small class="text-success">320,00kg em estoque</small>
+              </div>
+            </div> 
+          </div>
+          <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6 d-flex align-items-stretch">
+            <div class="card text-center bg-light">
+              <a href="" class="position-absolute right-0 p-2 text-danger">
+                <i class="bi bi-suit-heart"></i>
+              </a>
+              <img src="../img/Invocacao_Do_Mal.jpg" alt="Invocacao do mal" class="card-img-top">
+              <div class="card-header">
+                R$ 500,00
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">Banana Prata</h5>
+                <p class="card-text truncate-3l">
+                  Banana prata direto da terra
+                </p>
+              </div>
+              <div class="card-footer">
+                <form class="d-block">
+                  <button class="btn btn-danger">
+                    Add ao Carrinho
+                  </button>
+                </form>
+                <small class="text-success">320,00kg em estoque</small>
+              </div>
+            </div> 
+          </div>
+          <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6 d-flex align-items-stretch">
+            <div class="card text-center bg-light">
+              <a href="" class="position-absolute right-0 p-2 text-danger">
+                <i class="bi bi-suit-heart"></i>
+              </a>
+              <img src="../img/Invocacao_Do_Mal.jpg" alt="Invocacao do mal" class="card-img-top">
+              <div class="card-header">
+                R$ 500,00
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">Banana Prata</h5>
+                <p class="card-text truncate-3l">
+                  Banana prata direto da terra
+                </p>
+              </div>
+              <div class="card-footer">
+                <form class="d-block">
+                  <button class="btn btn-danger">
+                    Add ao Carrinho
+                  </button>
+                </form>
+                <small class="text-success">320,00kg em estoque</small>
+              </div>
+            </div> 
+          </div>
+          <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6 d-flex align-items-stretch">
+            <div class="card text-center bg-light">
+              <a href="" class="position-absolute right-0 p-2 text-danger">
+                <i class="bi bi-suit-heart"></i>
+              </a>
+              <img src="../img/Invocacao_Do_Mal.jpg" alt="Invocacao do mal" class="card-img-top">
+              <div class="card-header">
+                R$ 500,00
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">Banana Prata</h5>
+                <p class="card-text truncate-3l">
+                  Banana prata direto da terra
+                </p>
+              </div>
+              <div class="card-footer">
+                <form class="d-block">
+                  <button class="btn btn-danger">
+                    Add ao Carrinho
+                  </button>
+                </form>
+                <small class="text-success">320,00kg em estoque</small>
+              </div>
+            </div> 
+          </div>
+          <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6 d-flex align-items-stretch">
+            <div class="card text-center bg-light">
+              <a href="" class="position-absolute right-0 p-2 text-danger">
+                <i class="bi bi-suit-heart"></i>
+              </a>
+              <img src="../img/Invocacao_Do_Mal.jpg" alt="Invocacao do mal" class="card-img-top">
+              <div class="card-header">
+                R$ 500,00
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">Banana Prata</h5>
+                <p class="card-text truncate-3l">
+                  Banana prata direto da terra
+                </p>
+              </div>
+              <div class="card-footer">
+                <form class="d-block">
+                  <button class="btn btn-danger">
+                    Add ao Carrinho
+                  </button>
+                </form>
+                <small class="text-success">320,00kg em estoque</small>
+              </div>
+            </div> 
+          </div>
+          <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6 d-flex align-items-stretch">
+            <div class="card text-center bg-light">
+              <a href="" class="position-absolute right-0 p-2 text-danger">
+                <i class="bi bi-suit-heart"></i>
+              </a>
+              <img src="../img/Invocacao_Do_Mal.jpg" alt="Invocacao do mal" class="card-img-top">
+              <div class="card-header">
+                R$ 500,00
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">Banana Prata</h5>
+                <p class="card-text truncate-3l">
+                  Banana prata direto da terra
+                </p>
+              </div>
+              <div class="card-footer">
+                <form class="d-block">
+                  <button class="btn btn-danger">
+                    Add ao Carrinho
+                  </button>
+                </form>
+                <small class="text-success">320,00kg em estoque</small>
+              </div>
+            </div> 
+          </div>
+          <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6 d-flex align-items-stretch">
+            <div class="card text-center bg-light">
+              <a href="" class="position-absolute right-0 p-2 text-danger">
+                <i class="bi bi-suit-heart"></i>
+              </a>
+              <img src="../img/Invocacao_Do_Mal.jpg" alt="Invocacao do mal" class="card-img-top">
+              <div class="card-header">
+                R$ 500,00
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">Banana Prata</h5>
+                <p class="card-text truncate-3l">
+                  Banana prata direto da terra
+                </p>
+              </div>
+              <div class="card-footer">
+                <form class="d-block">
+                  <button class="btn btn-danger">
+                    Add ao Carrinho
+                  </button>
+                </form>
+                <small class="text-success">320,00kg em estoque</small>
+              </div>
+            </div> 
+          </div>
+          <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6 d-flex align-items-stretch">
+            <div class="card text-center bg-light">
+              <a href="" class="position-absolute right-0 p-2 text-danger">
+                <i class="bi bi-suit-heart"></i>
+              </a>
+              <img src="../img/Invocacao_Do_Mal.jpg" alt="Invocacao do mal" class="card-img-top">
+              <div class="card-header">
+                R$ 500,00
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">Banana Prata</h5>
+                <p class="card-text truncate-3l">
+                  Banana prata direto da terra
+                </p>
+              </div>
+              <div class="card-footer">
+                <form class="d-block">
+                  <button class="btn btn-danger">
+                    Add ao Carrinho
+                  </button>
+                </form>
+                <small class="text-success">320,00kg em estoque</small>
+              </div>
+            </div> 
+          </div>
+          <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6 d-flex align-items-stretch">
+            <div class="card text-center bg-light">
+              <a href="" class="position-absolute right-0 p-2 text-danger">
+                <i class="bi bi-suit-heart"></i>
+              </a>
+              <img src="../img/Invocacao_Do_Mal.jpg" alt="Invocacao do mal" class="card-img-top">
+              <div class="card-header">
+                R$ 500,00
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">Banana Prata</h5>
+                <p class="card-text truncate-3l">
+                  Banana prata direto da terra
+                </p>
+              </div>
+              <div class="card-footer">
+                <form class="d-block">
+                  <button class="btn btn-danger">
+                    Add ao Carrinho
+                  </button>
+                </form>
+                <small class="text-success">320,00kg em estoque</small>
+              </div>
+            </div> 
+          </div>
+          <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6 d-flex align-items-stretch">
+            <div class="card text-center bg-light">
+              <a href="" class="position-absolute right-0 p-2 text-danger">
+                <i class="bi bi-suit-heart-fill"></i>
+              </a>
+              <img src="../img/Invocacao_Do_Mal.jpg" alt="Invocacao do mal" class="card-img-top">
+              <div class="card-header">
+                R$ 500,00
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">Banana Prata</h5>
+                <p class="card-text truncate-3l">
+                  Banana prata direto da terra
+                </p>
+              </div>
+              <div class="card-footer">
+                <div>
+                  <button class="btn btn-light disabled" disabled>
+                    <small>Reabastecendo Estoque</small>
+                  </button>
+                </div>
+                <small class="text-danger">Produto Esgotado</small>
+              </div>
+            </div> 
+          </div>
+          
+          
+        </div> 
+        <hr class="mt-3">
+        <div class="row pb-4">
+          <div class="col-12">
+              <div class="d-flex flex-row-reverse justify-content-center justify-content-md-start">
+                <form class="ml-3 d-inline-block">
+                  <select class="form-select form-select-sm">
+                    <option>Ordenar pelo nome</option>
+                    <option>Ordenar pelo maior preço</option>
+                    <option>Ordenar pelo menor preço</option>
+                  </select>
+                </form>
+                <nav class="d-inline-block">
+                  <ul class="pagination pagination-sm my-0">
+                    <li class="page-item disabled">
+                      <button class="page-link">1</button>
+                    </li>
+                    <li class="page-item">
+                      <button class="page-link">2</button>
+                    </li>
+                    <li class="page-item">
+                      <button class="page-link">3</button>
+                    </li>
+                    <li class="page-item">
+                      <button class="page-link">4</button>
+                    </li>
+                    <li class="page-item">
+                      <button class="page-link">5</button>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+          </div>
+        </div>
+  </div>
+</main>
 </body>
 </html>
